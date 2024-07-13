9eab593c892a0e653ef5dff87aad8a3a7f075309
JoaoVitorVian <joaovitorvian@gmail.com>
2024-07-13 19:21:50 -0300

    adjusting server.ts and adding route to create invite

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,48 @@
-import fastify from "fastify";	
-import { prisma } from "./lib/prisma";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
-import { createTrip } from "./routes/create-trip";
-import { confirmTrip } from "./routes/confirm-trip";
+import fastify from 'fastify'
 import cors from '@fastify/cors'
-import { confirmParticipant } from "./routes/confirm-participant";
-import { getActivities } from "./routes/get-activities";
-import { createActivity } from "./routes/create-activity";
+import { createTrip } from './routes/create-trip'
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { confirmTrip } from './routes/confirm-trip'
+import { confirmParticipant } from './routes/confirm-participant'
+import { createActivity } from './routes/create-activity'
+import { getActivities } from './routes/get-activities'
+import { createLink } from './routes/create-link'
+import { getLinks } from './routes/get-links'
+import { getParticipants } from './routes/get-participants'
+import { createInvite } from './routes/create-invite'
+import { updateTrip } from './routes/update-trip'
+import { getTripDetails } from './routes/get-trip-details'
+import { getParticipant } from './routes/get-participant'
+import { errorHandler } from './error-handler'
+import { env } from './env'
 
-const app = fastify();
+const app = fastify()
 
 app.register(cors, {
-    origin: '*'
+  origin: '*',
 })
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.register(createTrip);
-app.register(confirmTrip);
-app.register(confirmParticipant);
-app.register(getActivities);
-app.register(createActivity);
+app.setErrorHandler(errorHandler)
 
-app.listen({port : 3333}).then(() => {  
-    console.log("listening on port");
-});
+app.register(createTrip)
+app.register(confirmTrip)
+app.register(confirmParticipant)
+app.register(createActivity)
+app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
+app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('Server running!')
+})
